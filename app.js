a50513b7f560d6de7b9483165b11db6b03d4bd5f
Jonelle Adona <jonelle.adona@knights.ucf.edu>
2020-11-25 00:02:09 -0500

    Add prompt question array

--- app.js
+++ app.js
@@ -14,6 +14,24 @@ const render = require("./lib/htmlRenderer");
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the employees name?"
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the ID number?"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the email address?"
+  }
+];
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
